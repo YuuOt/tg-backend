fb2656557c88166ee595a9eb2345f96bff8dacdb
YuuOt <zzloypchel@gmail.com>
2024-05-15 22:22:09 +0400

    1

--- index.js
+++ index.js
@@ -34,49 +34,29 @@ const getProductsFromFirestore = async () => {
 app.use(express.json());
 app.use(cors());
 
-bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-  const text = msg.text;
-  console.log(msg);
-  
-
-  if(text === '/start') {
-        await bot.sendMessage(chatId,'Ниже появится кнопка, заполнить форму', {
-            reply_markup: {
-                keyboard: [
-                    [{text: 'Заполнить форму', web_app: {url: webAppUrl +'/form'}}]
-                ]
-            }
-        })
-        await bot.sendMessage(chatId,'Команда для поиска товара: /search "название товара"')
-        await bot.sendMessage(chatId,'Заходите в наш интернет магазин по кнопке ниже', {
-            reply_markup: {
-                inline_keyboard: [
-                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
-                ]
-            }
-        })
-  }
-
-
-    if(msg?.web_app_data?.data) {
-        try{
-            const data = JSON.parse(msg?.web_app_data?.data)
-
-
-            await bot.sendMessage(chatId,'Спасибо за обратную связь!')
-            await bot.sendMessage(chatId,'Ваша страна: '+ data?.country);
-            await bot.sendMessage(chatId,'Почта: '+ data?.street)
-        } catch (e) {
-            console.log(e);
-        }
-        
-    }
-
-});
-
 const chatState = {}; // Объект для хранения состояний чатов
 
+// Обработчик команды /start
+bot.onText(/\/start/, async (msg) => {
+  const chatId = msg.chat.id;
+  await bot.sendMessage(chatId, 'Ниже появится кнопка, чтобы заполнить форму', {
+    reply_markup: {
+      keyboard: [
+        [{ text: 'Заполнить форму', web_app: { url: webAppUrl + '/form' } }]
+      ]
+    }
+  });
+  await bot.sendMessage(chatId, 'Команда для поиска товара: /search "название товара"');
+  await bot.sendMessage(chatId, 'Заходите в наш интернет магазин по кнопке ниже', {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: 'Сделать заказ', web_app: { url: webAppUrl } }]
+      ]
+    }
+  });
+});
+
+// Обработчик команды /search
 bot.onText(/\/search/, async (msg) => {
   const chatId = msg.chat.id;
   const searchQuery = msg.text.split(' ')[1];
@@ -158,6 +138,23 @@ bot.on('message', async (msg) => {
       console.error('Error searching for products:', error);
       bot.sendMessage(chatId, 'Произошла ошибка при поиске товаров.');
     }
+  } else if (!text.startsWith('/')) {
+    // Если сообщение не является командой, отправляем список команд
+    bot.sendMessage(chatId, 'Пожалуйста, используйте одну из следующих команд:\n' +
+      '/start - Начать взаимодействие\n' +
+      '/search "название товара" - Поиск товара');
+  }
+
+  if (msg?.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg?.web_app_data?.data);
+
+      await bot.sendMessage(chatId, 'Спасибо за обратную связь!');
+      await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+      await bot.sendMessage(chatId, 'Почта: ' + data?.street);
+    } catch (e) {
+      console.log(e);
+    }
   }
 });
 
@@ -171,24 +168,24 @@ app.get('/productlist', async (req, res) => {
 });
 
 app.post('/web-data', async (req, res) => {
-    const {queryId, products, totalPrice,tg} =req.body;
-    try {
-      await bot.answerWebAppQuery(queryId,{
-            type: 'article',
-            id: queryId,
-            title: 'Успешная покупка',
-            input_message_content: {message_text: 'Вы оформили заказа, ' + tg.MainButton.text }
-        })
-        return res.status(200).json({});
-    } catch(e){
-        await bot.answerWebAppQuery(queryId,{
-            type: 'article',
-            id: queryId,
-            title: 'Не удалось приобрести товар',
-            input_message_content: {message_text: 'Не удалось приобрести товар'}
-        })
-        return res.status(500).json({});
-    }
+  const { queryId, products, totalPrice, tg } = req.body;
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: 'Успешная покупка',
+      input_message_content: { message_text: 'Вы оформили заказ, ' + tg.MainButton.text }
+    });
+    return res.status(200).json({});
+  } catch (e) {
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: 'Не удалось приобрести товар',
+      input_message_content: { message_text: 'Не удалось приобрести товар' }
+    });
+    return res.status(500).json({});
+  }
 });
 
 const PORT = 8000;
