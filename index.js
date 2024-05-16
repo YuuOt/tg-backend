675f4c8ff0dbe49124703baf9ea5d7a2096a52cb
YuuOt <zzloypchel@gmail.com>
2024-05-17 00:14:27 +0400

    1

--- index.js
+++ index.js
@@ -151,11 +151,19 @@ app.post('/api/login', (req, res) => {
 
 // Middleware для проверки JWT
 function authenticateToken(req, res, next) {
-  const token = req.headers['authorization'];
-  if (!token) return res.sendStatus(401);
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    console.log('No token provided');
+    return res.sendStatus(401);
+  }
 
   jwt.verify(token, secretKey, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) {
+      console.log('Token verification failed', err);
+      return res.sendStatus(403);
+    }
     req.user = user;
     next();
   });
